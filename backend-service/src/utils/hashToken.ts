3ec219f50ffc62c8e452511dc23b923db26de33c
ndambodavid <davidndambo6@gmail.com>
2024-07-05 11:30:08 +0300

    auth and error middleware setup

--- backend-service/src/utils/hashToken.ts
+++ backend-service/src/utils/hashToken.ts
@@ -1,5 +1,10 @@
 import crypto from 'crypto'
 
+/**
+ * hash the jwt token before saving to database
+ * @param token 
+ * @returns 
+ */
 export default function hashToken(token: string) {
     return crypto.createHash('sha512').update(token).digest('hex');
   }
